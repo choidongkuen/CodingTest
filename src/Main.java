d995d5aa2962de99484ae77f9775e888ca29dff4
choidongkuen <danaver12@daum.net>
2022-09-27 14:41:15 +0900

    commit 09/27

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,16 @@
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 // 완전 탐색으로 풀기에는 O(N^2) 걸린다.
 // 투포인터로 풀자!
 
 public class Main {
-    public static final int MAX_N = 100000; // 10의 5승
-    public static final int MAX_S = 100000000; // 10의 8승
-    public static int[] arr = new int[MAX_N];
     public static void main(String[] args) {
+        int[] arr1 = {1,23,4,1};
+        int[] arr2 = {1,2};
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 원소 갯수
-        int s = sc.nextInt(); // 기준이 되는 값
+        Integer[] refArr = Arrays.stream(arr1).boxed().toArray(Integer[] :: new); // 기본형 배열 - > 참조형 배열
 
-        for(int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-
-        int p1 = 0;
-        int p2 = 0;
-        int sum = arr[0];
-        int maxLen = n;
-
-        boolean canMake = false;
-
-        while(p1 <= p2 && p2 < n){
-
-            if(sum >= s){
-                maxLen = Math.min(maxLen,p2 - p1 + 1);
-                sum -= arr[p1 ++];
-                canMake = true;
-            }else{
-                p2++;
-                sum += arr[p2];
-            }
-        }
-        if(!canMake)
-            System.out.println(0);
-        else
-            System.out.println(maxLen);
     }
 }
 
